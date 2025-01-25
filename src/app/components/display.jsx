a0dac08dd2d662c8bc79105fbad729444dd2a66e
Jason A. Wise <jasonandrewwise@gmail.com>
2025-01-24 21:25:34 -0500

    setting timer and changing button color

--- src/app/components/display.jsx
+++ src/app/components/display.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 export default function Display() {
   const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(25);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -15,14 +16,30 @@ export default function Display() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 25));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [currentSentenceIndex]);
+
   const handleNextClick = () => {
     setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    setTimeLeft(25);
   };
+
+  useEffect(() => {
+    console.log(timeLeft);
+  }, [timeLeft]);
 
   return (
     <div>
       <p className=" text-3xl">{sentences[currentSentenceIndex]}</p>
-      <button className="mt-4" onClick={handleNextClick}>
+      <button
+        className="mt-4 bg-green-600 px-4 py-2 rounded-md"
+        onClick={handleNextClick}
+      >
         Next
       </button>
     </div>
